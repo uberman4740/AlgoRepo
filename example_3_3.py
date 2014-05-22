be9b614ecc4bf84ce2172aa5bb69a0199713aa45
RoadWarrior <javier.macro.trader@gmail.com>
2014-05-22 09:19:19 -0500

    updated 20140522

--- example_3_3.py
+++ example_3_3.py
@@ -1,113 +1,136 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from functions import *
-from numpy.matlib import repmat
-import numpy as np
-
-# Implementation of the first part of example 3.1 in Ernest Chang's 
-# book Algorithmic trading : winning strategies and their rationale.
-#
-# Price Spread
 
 if __name__ == "__main__":
-   
-    #import data from CSV file
+    import pandas as pd
+    from numpy.matlib import repmat
+    import matplotlib.pyplot as plt
+    from functions import *
+    from numpy.matlib import repmat
+    import numpy as np
+
+    import prettyplotlib as ppl
+    ################################################################
+    # import data from CSV file
+    ################################################################
     root_path = 'C:/Users/javgar119/Documents/Python/Data/'
     # the paths
     # MAC: '/Users/Javi/Documents/MarketData/'
     # WIN: 'C:/Users/javgar119/Documents/Python/Data/'
-    filename = 'EWC_EWA_daily.csv'   #version 2 is bloomberg data
+    filename = 'EWC_EWA_daily.csv'   
     full_path = root_path + filename
     data = pd.read_csv(full_path, index_col='Date')
-   
-    y_ticket = 'EWC'
+    #print(data.head(10))
     x_ticket = 'EWA'
+    y_ticket = 'EWC'
     
-    y = data[y_ticket]
-    x = data[x_ticket]
+    y = np.asarray(data[y_ticket])
+    x = pd.DataFrame(data[x_ticket])
+    index = x.index
+
+    ################################################################
+    # Kalman filter implementation
+    ################################################################
+    # Augment x with ones to  accomodate possible offset in the 
+    # regression between y vs x
+    x['ones'] = np.ones(len(x))
+    x = np.asarray(x)
     
-    entryZscore = 1
-    exitZscore = 1
+    # delta=1 gives fastest change in beta, delta=0.000....
+    # 1 allows no change (like traditional linear regression).
+    delta=0.0001
+    
+    yhat=np.zeros(shape=(len(x),1))     # measurement prediction
+    e=np.zeros(shape=(len(x),1))        # measurement prediction error
+    Q=np.zeros(shape=(len(x),1))        # measurement prediction error variance
+  
+    # For clarity, we denote R(t|t) by P(t).
+    # initialize R, P and beta.
+    R = np.zeros((2,2));
+    P = np.zeros((2,2));
+    beta = np.zeros((2, len(x)))
+    Vw = delta/(1-delta)*np.eye(2)
+    Ve=0.001
 
-    # lookback period for calculating the dynamically changing
-    lookback = 20
-    modelo = pd.ols(y=y, x=x, window_type='rolling', window=lookback)
-    data = data[lookback-1:]
-    betas = modelo.beta
+    # Given initial beta and R (and P)
+    for t in range(len(y)):
+        if t > 0:
+            beta[:, t] = beta[:, t-1]                   # state prediction. Equation 3.7
+            R= P + Vw                                   # state covariance prediction. Equation 3.8
+            yhat[t] = x[t, :].dot(beta[:, t])           # measurement prediction. Equation 3.9
+            Q[t] = x[t, :].dot(R).dot(x[t, :].T) + Ve   # measurement variance prediction. Equation 3.10
+            # Observe y(t)
+            e[t] = y[t] - yhat[t]                       # measurement prediction error
+            K = R.dot(x[t, :].T) / Q[t]                 # Kalman gain
+            beta[:, t] = beta[:, t] + K * e[t]          # State update. Equation 3.11
+            P= R - K * x[t, :] * R                      # State covariance update. Euqation 3.12
 
-    # calculate the number of units for the strategy in the form
-    # y-beta*x
-    yport = pd.DataFrame(data[y_ticket] - (betas['x'] * data[x_ticket]))
-    # z score calculation
-    moving_mean = pd.rolling_mean(yport, window=lookback)
-    moving_std = pd.rolling_std(yport,window=lookback)
-    Zscore = (yport - moving_mean) / moving_std
+    sqrt_Q = np.sqrt(Q)
+    beta = pd.DataFrame(beta.T, index= index, columns=('x', 'intercept'))
+    e = pd.DataFrame(e, index=index)
 
+    ################################################################
+    # trading strategy
+    ################################################################
     # trade signal 
-    long_entry = Zscore < -entryZscore
-    long_exit = Zscore >= -entryZscore
-    short_entry = Zscore > entryZscore
-    short_exit = Zscore <= entryZscore
+    long_entry = e < -sqrt_Q   # a long position means we should buy EWC
+    long_exit = e > -sqrt_Q
+    short_entry = e > sqrt_Q
+    short_exit =  e < sqrt_Q
     
-
-    
-    numunits_long= np.zeros((len(yport),1))
+    numunits_long= np.zeros((len(data),1))
     numunits_long = pd.DataFrame(np.where(long_entry,1,0))
-    
-    
-    
-    numunits_short= np.zeros((len(yport),1))
+    numunits_short= np.zeros((len(data),1))
     numunits_short = pd.DataFrame(np.where(short_entry,-1,0))
-
     numunits = numunits_long + numunits_short
-    
-    #print(numunits.tail(200))
  
-    # compute the $ position for each asset
+    #compute the $ position for each asset
     AA = pd.DataFrame(repmat(numunits,1,2))
-    BB = pd.DataFrame(-betas['x'])
-    BB['ones'] = np.ones((len(betas)))
-    
+    #print(AA.head(10))
+    BB = pd.DataFrame(-beta['x'])
+    BB['ones'] = np.ones((len(beta)))
+    #print(BB.head(10))
     position = multiply(multiply(AA, BB), data)
-
+    #print(position.head(10))
     #print (BB.head(50))
 
     # compute the daily pnl in $$
-    
     pnl = sum(multiply(position[:-1], divide(diff(data,axis = 0), data[:-1])),1)
- 
-        
     # gross market value of portfolio
     mrk_val = pd.DataFrame.sum(abs(position), axis=1)
     mrk_val = mrk_val[:-1]
-   
     # return is P&L divided by gross market value of portfolio
     rtn = pnl / mrk_val
+    # cumulative return and smoothing of series for plotting
     acum_rtn = pd.DataFrame(cumsum(rtn))
     acum_rtn = acum_rtn.fillna(method='pad')
     # compute performance statistics
     sharpe = (np.sqrt(252)*np.mean(rtn)) / np.std(rtn)
     APR = np.prod(1+rtn)**(252/len(rtn))-1
     
-    ##################################################
+    ################################################################
     # print the results
-    ##################################################
-    print('Price spread Sharpe: {:.4}'.format(sharpe))
-    print('Price Spread APR: {:.4%}'.format(APR))
+    ################################################################
+    print('Sharpe: {:.4}'.format(sharpe))
+    print('APR: {:.4%}'.format(APR))
     
-
-    #*************************************************
+    ################################################################
     # plotting the chart
-    #*************************************************    
-    #plot of numunits
+    ################################################################
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(acum_rtn)
-    ax.set_title('{}-{} Bollinger Band     Acum Return'.format(x_ticket, y_ticket))
+    ax.set_title('{}-{} Price Ratio CumRet\n(Using Kalman filter)'.format(x_ticket, y_ticket))
     ax.set_xlabel('Data points')
-    ax.set_ylabel('acumm rtn')
-    ax.text(1000, 0, 'Sharpe: {:.4}'.format(sharpe))
-    ax.text(1000, -0.03, 'APR: {:.4%}'.format(APR))
-    
+    ax.set_ylabel('cum rtn')
+    ax.text(1200, 0.2, 'Sharpe: {:.4}'.format(sharpe))
+    ax.text(1200, 0.15, 'APR: {:.4%}'.format(APR))
     plt.show()
     
+    # chart example with prettyplotlib
+    #fig, ax = plt.subplots(1)
+    #y = acum_rtn
+    #x = acum_rtn.index
+    #i = 'Price Ratio CumRet\n(Using Kalman filter)'
+    #ppl.plot(x, y, label=str(i), linewidth=0.75)
+    #ppl.legend(ax)
+    #fig.savefig('kelman strat.png')
+
--- kalman.py
+++ kalman.py
@@ -1,128 +1,79 @@
+from numpy import dot, sum, tile, linalg 
+from numpy.linalg import inv, det 
 
-import pandas as pd
-from numpy import diag, eye, zeros, pi, exp, dot, sum, tile, linalg, array
-from numpy import log as logg
-from numpy.linalg import inv, det
-from numpy.random import randn
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-
-
-
-
-
-# Implementation of a Kalman filter for time series analysis.
-# Ideas adopted from http://hal.archives-ouvertes.fr/docs/00/43/38/86/PDF/Laaraiedh_PythonPapers_Kalman.pdf
-
-# Algorithm plots both the observed price and the signal produced
-# by the Kalman filter for assets in the universe.
-
-def initialize(x, y):
-    # Set total number of securities
-    
-    total_securites = 2
-
-    
-    X = np.zeros((total_securites*2,1))
-    P = np.diag(np.zeros(total_securites*2))   
-    A = array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0,0]])    
-    Q = eye(X.shape[0])
-    B = eye(X.shape[0])
-    U = zeros((X.shape[0],1))
-    #Y = array([[0], [0]])
-    Y = array(x)
-    H = array([[1, 0, 0, 0], [0, 1, 0, 0]])
-    R = eye(Y.shape[0])
-
-    # Apply the Kalman filter
-    # Prediction step 
-    N_iter = 50 
-
-    for i in range(N_iter):
-        
-        (X, P) = kf_predict(X, P, A, Q, B, U)
-        # Update step
-        (X, P, K, IM, IS, LH) = kf_update(X, P, Y, H, R)  
-        # Record observed price data
-        
-      
-       
-    print(X)
-    
-# PREDICTION STEP
-
-# X is the mean state estimate of the previous step (k-1)
-# P is the state covariance matrix of the previous step (k-1)
-# A is the transition nxn matrix
-# Q is the process noise covariance matrix
-# B is the input effect matrix
-# U is the control input
 
 def kf_predict(X, P, A, Q, B, U): 
  X = dot(A, X) + dot(B, U) 
  P = dot(A, dot(P, A.T)) + Q 
- return(X,P) 
+ return(X,P)
+ 
+def kf_update(X, P, Y, H, R): 
+ IM = dot(H, X) 
+ IS = R + dot(H, dot(P, H.T)) 
+ K = dot(P, dot(H.T, inv(IS))) 
+ X = X + dot(K, (Y-IM)) 
+ P = P - dot(K, dot(IS, K.T)) 
+ LH = gauss_pdf(Y, IM, IS) 
+ return (X,P,K,IM,IS,LH) 
+ 
+def gauss_pdf(X, M, S): 
+ if M.shape[1] == 1: 
+     DX = X - tile(M, X.shape[1]) 
+     E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0) 
+     E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+     P = exp(-E) 
+ elif X.shape[1] == 1: 
+     DX = tile(X, M.shape[1])- M 
+     E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0) 
+     E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+     P = exp(-E) 
+ else: 
+     DX = X-M 
+     E = 0.5 * dot(DX.T, dot(inv(S), DX)) 
+     E = E + 0.5 * M.shape[0] * log(2 * pi) + 0.5 * log(det(S)) 
+     P = exp(-E) 
+ 
+ return (P[0],E[0])   
+ 
+ 
+ 
+"""
+Implementation of Kalman filter as propouse in
+http://hal.archives-ouvertes.fr/docs/00/43/38/86/PDF/Laaraiedh_PythonPapers_Kalman.pdf
+Still not working for me
+"""
 
-
-# UPDATE STEP
-
-# At time step k, this update computes the posterior mean X and 
-# covariance P of the system state given a new measurement Y. 
-
-# Y is the measurement vector
-# H is the measurement matrix
-# R is the measurement covariance matrix
-# K is the Kalman Gain matrix
-# IM is the mean of the predictive distribution of Y
-# IS is the covariance of the predictive mean of Y
-# LH is the predictive probability (lieklihood) of measurement
-
-def kf_update(X, P, Y, H, R):
-    IM = dot(H, X)
-    IS = R + dot(H, dot(P, H.T))
-    K = dot(P, dot(H.T, inv(IS)))
-    X = X + dot(K, (Y-IM))
-    P = P - dot(K, dot(IS, K.T))
-    LH = gauss_pdf(Y, IM, IS)
-    return (X,P,K,IM,IS,LH)
+if __name__ == "__main__": 
+    from numpy import *
+    from numpy.linalg import inv 
+    from numpy.random import randn
+    #time step of mobile movement 
+    dt = 0.1 
+     
+    # Initialization of state matrices 
+    X = array([[0.0], [0.0], [0.1], [0.1]]) 
+    P = diag((0.01, 0.01, 0.01, 0.01)) 
+    A = array([[1, 0, dt , 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]]) 
+    Q = eye(X.shape[0]) 
+    B = eye(X.shape[0]) 
+    U = zeros((X.shape[0],1))
+    # Measurement matrices 
+    Y = array([[X[0,0] + abs(randn(1)[0])], [X[1,0] + abs(randn(1)[0])]]) 
+    H = array([[1, 0, 0, 0], [0, 1, 0, 0]]) 
+    R = eye(Y.shape[0]) 
+     
+    # Number of iterations in Kalman Filter 
+    N_iter = 50
+     
+    # Applying the Kalman Filter 
+    for i in arange(0, N_iter): 
+        (X, P) = kf_predict(X, P, A, Q, B, U) 
+        (X, P, K, IM, IS, LH) = kf_update(X, P, Y, H, R) 
+        Y = array([[X[0,0] + abs(0.1 * randn(1)[0])],[X[1, 0] + abs(0.1 * randn(1)[0])]]) 
+     
     
-def gauss_pdf(X, M, S):
-    if M.shape[1] == 1:
-        DX = X - tile(M, X.shape[1])
-        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
-        E = E + 0.5 * M.shape[0] * logg(2 * pi) + 0.5 * logg(det(S))
-        P = exp(-E)
-    elif X.shape[1] == 1:
-        DX = tile(X, M.shape[1])- M
-        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
-        E = E + 0.5 * M.shape[0] * logg(2 * pi) + 0.5 * logg(det(S))
-        P = exp(-E)
-    else:
-        DX = X-M
-        E = 0.5 * dot(DX.T, dot(inv(S), DX))
-        E = E + 0.5 * M.shape[0] * logg(2 * pi) + 0.5 * logg(det(S))
-        P = exp(-E)
-    return (P[0],E[0])
     
-
-
-if __name__ == "__main__":
-    import pandas as pd
-    import numpy as np
-       #import data from CSV file
-    root_path = '/Users/Javi/Documents/MarketData/'
-    # the paths
-    # MAC: '/Users/Javi/Documents/MarketData/'
-    # WIN: 'C:/Users/javgar119/Documents/Python/Data/'
-    filename = 'EWC_EWA_daily.csv'   #version 2 is bloomberg data
-    full_path = root_path + filename
-    data = pd.read_csv(full_path, index_col='Date')
-   
-    y_ticket = 'EWC'
-    x_ticket = 'EWA'
     
-    y = data[y_ticket]
-    x = data[x_ticket]
     
-    initialize(x=x, y=y)
+     
+      
